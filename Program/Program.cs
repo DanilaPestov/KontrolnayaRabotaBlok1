24a95a8568720c7643c1eecaf34a3b8fb3865391
DanilaPestov <89147750075@mail.ru>
2022-06-21 02:23:39 +1000

    Program

--- Program/Program.cs
+++ Program/Program.cs
@@ -1,3 +1,32 @@
-﻿//Написать программу, которая из имеющегося массива строк формирует массив из строк, 
-//длина которых меньше либо равна 3 символа. 
-//Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма
+﻿string[] mass = { "Hello", "2", "world", ":-)", "1", "22", "333", "4444", "5" };
+
+string Print(string[] array)
+{
+    string res = string.Empty;
+    res = "[ ";
+    for (int i = 0; i < array.Length; i++)
+    {
+        res += $"{array[i]} +  ";
+    }
+    res += " ]";
+    return res;
+}
+
+string[] Result(string[] array)
+{
+    string[] result = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            result[i] = array[i];
+            Console.Write(result[i] + " ");
+        }
+    }
+    Console.WriteLine();
+    return result;
+}
+
+Print(mass);
+Console.WriteLine("новый массив с 3 и менее символами: ");
+Result(mass);
